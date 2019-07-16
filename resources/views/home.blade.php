c41de47795d6a96766fac11100bb0caaccf08193
Maximiliano Flores <sergio@hiringroom.com>
2019-07-16 17:47:36 -0300

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Welcome to the most amazing blog!
                 </div>
             </div>
         </div>
